1b60911696ed6132e09914e7d1e57bd6a9bcea39
Aryan Singh <aquiveal@gmail.com>
2026-06-28 18:03:23 +0530

    refactor!: rename Campaign Multi Channel Schedule to Cadence
    Move the Campaign Multi Channel Schedule doctype and logic to the Cadence module to align with project rebranding.
    
    BREAKING-CHANGE: CampaignMultiChannelSchedule has been removed and replaced by CadenceMultiChannelSchedule.

--- frappe_cadence/cadence/doctype/cadence_multi_channel_schedule/cadence_multi_channel_schedule.py
+++ frappe_cadence/cadence/doctype/cadence_multi_channel_schedule/cadence_multi_channel_schedule.py
@@ -6,7 +6,7 @@
 from frappe.model.document import Document
 
 
-class CampaignMultiChannelSchedule(Document):
+class CadenceMultiChannelSchedule(Document):
 	# begin: auto-generated types
 	# This code is auto-generated. Do not modify anything in this block.
 
