bea9a78477949dc1e313435320bbe4eee97d6482
Aryan Singh <aquiveal@gmail.com>
2026-07-05 18:11:45 +0530

    feat: add optimize and predict buttons to sms template
    Add custom buttons to the SMS Template form to trigger optimization and prediction workflows. This allows users to access advanced analytics and content improvement tools directly from the document interface.

--- frappe_cadence/cadence/doctype/sms_template/sms_template.js
+++ frappe_cadence/cadence/doctype/sms_template/sms_template.js
@@ -9,5 +9,35 @@ frappe.ui.form.on('SMS Template', {
                 }
             };
         });
-    }
+    },
+	refresh: function(frm) {
+		frm.add_custom_button(__("Optimize"), function() {
+			frappe.call({
+				method: "frappe_cadence.utils.sift.optimize",
+				args: {
+					template_doctype: frm.doc.doctype,
+					template_name: frm.doc.name
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						frm.reload_doc();
+					}
+				}
+			});
+		});
+		frm.add_custom_button(__("Predict"), function() {
+			frappe.call({
+				method: "frappe_cadence.utils.sift.predict",
+				args: {
+					template_doctype: frm.doc.doctype,
+					template_name: frm.doc.name
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						frm.reload_doc();
+					}
+				}
+			});
+		});
+	}
 });
