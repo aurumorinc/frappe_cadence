d9c8f53a799b39d20d20a49e04ed38bdbdba5ba7
Aryan Singh <aquiveal@gmail.com>
2026-07-05 18:33:54 +0530

    feat: implement optimize and predict buttons
    Replace placeholder messages with functional API calls to the Sift utility. These buttons now trigger server-side optimization and prediction processes for the current email template and reload the document upon completion.

--- frappe_cadence/public/js/email_template.js
+++ frappe_cadence/public/js/email_template.js
@@ -1,10 +1,32 @@
 frappe.ui.form.on("Email Template", {
 	refresh: function(frm) {
 		frm.add_custom_button(__("Optimize"), function() {
-			frappe.msgprint(__("Not implemented"));
+			frappe.call({
+				method: "frappe_cadence.utils.sift.optimize",
+				args: {
+					template_doctype: frm.doc.doctype,
+					template_name: frm.doc.name
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						frm.reload_doc();
+					}
+				}
+			});
 		});
-		frm.add_custom_button(__("Process"), function() {
-			frappe.msgprint(__("Not implemented"));
+		frm.add_custom_button(__("Predict"), function() {
+			frappe.call({
+				method: "frappe_cadence.utils.sift.predict",
+				args: {
+					template_doctype: frm.doc.doctype,
+					template_name: frm.doc.name
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						frm.reload_doc();
+					}
+				}
+			});
 		});
 	}
 });
