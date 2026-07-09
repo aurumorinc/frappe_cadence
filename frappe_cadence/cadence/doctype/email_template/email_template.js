95f9901681c20b76fe40e51f0dc2333bb754ac67
Aryan Singh <aquiveal@gmail.com>
2026-07-10 03:50:57 +0530

    refactor: move email template script to doctype
    Move the Email Template form script from the public directory to the doctype-specific location. This consolidates form customizations in the appropriate doctype directory and simplifies the module structure.
    
    The script functionality remains unchanged, maintaining the Optimize and Predict custom buttons with their respective API calls.

--- frappe_cadence/cadence/doctype/email_template/email_template.js
+++ frappe_cadence/cadence/doctype/email_template/email_template.js
@@ -1,13 +1,32 @@
-frappe.ui.form.on('Email Template', {
-    setup: function(frm) {
-        frm.set_query("reference_name", "annotations", function(doc, cdt, cdn) {
-            return {
-                query: "frappe_cadence.utils.enrichment.get_crm_leads",
-                filters: {
-                    "template_name": doc.name,
-                    "template_type": doc.doctype
-                }
-            };
-        });
-    }
+frappe.ui.form.on("Email Template", {
+	refresh: function(frm) {
+		frm.add_custom_button(__("Optimize"), function() {
+			frappe.call({
+				method: "frappe_cadence.utils.sift.optimize",
+				args: {
+					template_doctype: frm.doc.doctype,
+					template_name: frm.doc.name
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						frm.reload_doc();
+					}
+				}
+			});
+		});
+		frm.add_custom_button(__("Predict"), function() {
+			frappe.call({
+				method: "frappe_cadence.utils.sift.predict",
+				args: {
+					template_doctype: frm.doc.doctype,
+					template_name: frm.doc.name
+				},
+				callback: function(r) {
+					if (!r.exc) {
+						frm.reload_doc();
+					}
+				}
+			});
+		});
+	}
 });
